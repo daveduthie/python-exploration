482be1a761f0d7e7655895a87114d5342d230fa6
David Duthie <dave.a.duthie@gmail.com>
2017-07-08 17:41:50 +0800

    fix bfs, implement graph construction and matching

--- Airline.py
+++ Airline.py
@@ -1,5 +1,28 @@
 # python3
 
+'''
+Why are we stuck with the crappy API?
+
+1. We want fast lookup of 'reverse' edges in the graph (using the XOR 1 trick)
+    - Maybe solved by storing edge indices in dicts
+2. We might have multiple edges linking two nodes, or even edges from a node
+   to itself.
+    - This means we cannot simply say add_flow(from, to, flow) and hope
+     to hit the correct edge. (We might, but it would be by luck)
+    - How about storing the graph indices as:
+        [{1: #{1, 3, 5}, 2: #{7, 8, 10}},
+         {0: #{...}, 2: #{...}},
+         ...]
+
+Other OCD unhappiness:
+    
+1. Does MaxMatching really need to be a class?
+Calling g = m.build_graph() feels really weird.
+I'd like to kill the class and coordinate the call graph myself from another
+(extra-class) function.
+       
+'''
+
 import collections
 
 
@@ -44,7 +67,7 @@ class FlowGraph:
     def num_edges(self):
         return len(self.edges)
 
-    def get_edges_from(self, from_):
+    def get_edge_indices(self, from_):
         return self.graph[from_].items()
 
     def get_edge(self, id):
@@ -60,52 +83,71 @@ class FlowGraph:
         It turns out that id ^ 1 works for both cases. Think this through!'''
         self.edges[id].flow += flow
         self.edges[id ^ 1].flow -= flow
-
-    def breadth_first_search(self, from_, to):
-        '''Make a graph
-        >>> f = FlowGraph(4)
-        >>> f.add_edge(0, 1, 1)
-        >>> f.add_edge(0, 2, 1)
-        >>> f.add_edge(1, 3, 1)
-        >>> f.add_edge(2, 3, 1)
-        
-        Then do a BFS
-        >>> f.breadth_first_search(0, 3)
-        [0, 4]
+    
         '''
-        predecessor = [-1 for _ in range(self.num_edges())]
-        visited = [False for _ in range(self.num_vertices())]
-        visited[from_] = True
+        # aspirational API
+        def add_flow(_, from, to, flow):
+            idx = self.graph[from_][to]
+            self.edges[idx] += flow
+            self.edges[idx ^ 1] -= flow
+        '''
 
-        q = collections.deque()
-        q.append(from_)
 
-        while len(q) > 0:
-            i = q.pop()
-            e1 = self.edges[i]
-            for (k, v) in self.get_edges_from(e1.to):
-                e2 = self.edges[v]
-                if (not visited[k]) and (e2.capacity - e2.flow > 0):
-                    predecessor[v] = i
-                    visited[k] = True
-                    q.append(v)
+def breadth_first_search(graph, from_, to):
+    '''Make a graph
+    >>> f = FlowGraph(4)
+    >>> f.add_edge(0, 1, 1)
+    >>> f.add_edge(0, 2, 1)
+    >>> f.add_edge(1, 3, 1)
+    >>> f.add_edge(2, 3, 1)
+    
+    Then do a BFS
+    >>> breadth_first_search(f, 0, 3)
+    (1, [0, 4])
+    '''
+    predecessor = [-1 for _ in range(graph.num_edges())]
+    visited = [False for _ in range(graph.num_vertices())]
+    visited[from_] = True
 
-            # print(
-                # 'about to consider whether {0} --> {1} leads to the end'
-                # .format(e1.from_, e1.to))
+    q = collections.deque()
+    for (dest, idx) in graph.get_edge_indices(from_):
+        e = graph.edges[idx]
+        if (e.capacity - e.flow > 0):
+            q.append(idx)
+            predecessor[dest] = from_
 
-            if e1.to == to:
-                path = []
-                while True:
-                    path.append(i)
-                    # print('value of path: {}'.format(path))
-                    if self.edges[i].from_ == from_:
-                        # print('breaking')
-                        break
-                    i = predecessor[i]
+    while len(q) > 0:
+        i = q.pop()
+        e1 = graph.edges[i]
+        # print('examing edge: {0} --> {1}'.format(e1.from_, e1.to))
+        for (k, v) in graph.get_edge_indices(e1.to):
+            e2 = graph.edges[v]
+            # print('examing inner edge: {0} --> {1}'.format(e2.from_, e2.to))
+            if (not visited[k]) and (e2.capacity - e2.flow > 0):
+                # print('added inner edge to queue')
+                predecessor[v] = i
+                visited[k] = True
+                q.append(v)
 
-                # print('returning the path: {}'.format(path))
-                return path[::-1]
+        # print(
+            # 'about to consider whether {0} --> {1} leads to the end'
+            # .format(e1.from_, e1.to))
+
+        if e1.to == to:
+            # print('It does!')
+            min_flow = e1.capacity - e1.flow
+            path = []
+            while True:
+                path.append(i)
+                edge_cap = graph.edges[i].capacity - graph.edges[i].flow
+                min_flow = min(min_flow, edge_cap)
+                if graph.edges[i].from_ == from_:
+                    # print('returning the path: {}'.format(path))
+                    return (min_flow, path[::-1])
+                i = predecessor[i]
+
+    # print('returning empty path')
+    return (0, [])
 
 # Maybe I won't need this
 '''
@@ -120,9 +162,17 @@ def read_data():
 
 
 def max_flow(graph, from_, to):
-    flow = 0
-    # your code goes here
-    return flow
+    '''Returns a tuple: (total flow, modified graph)'''
+    total_flow = 0
+    while True:
+        (flow, path) = breadth_first_search(graph, from_, to)
+        # print('flow: {0} path: {1}'.format(flow, path))
+        if flow == 0:
+            break
+        for idx in path:
+            graph.add_flow(idx, flow)
+        total_flow += flow
+    return (total_flow, graph)
 
 
 class MaxMatching:
@@ -132,13 +182,39 @@ class MaxMatching:
         return adj_matrix
 
     def write_response(self, matching):
-        line = [str(-1 if x == -1 else x + 1) for x in matching]
+        line = [str(-1 if x == -1 else x) for x in matching]
         print(' '.join(line))
 
-    def find_matching(self, adj_matrix):
-        # Replace this code with an algorithm that finds the maximum
-        # matching correctly in all cases.
-        n = len(adj_matrix)
+    def build_graph(self, adj_matrix):
+        if not len(adj_matrix) > 0:
+            return
+        # build graph
+        num_flights = len(adj_matrix)
+        num_crews = len(adj_matrix[0])
+        g = FlowGraph(num_crews + num_flights + 2)
+        
+        # Add edges from crews to flights
+        for (f_idx, flight) in enumerate(adj_matrix, num_crews + 1):
+            for (c_idx, able) in enumerate(flight, 1):
+                if able:
+                    g.add_edge(c_idx, f_idx, 1)
+                    # print('added edge from {0} to {1}'.format(c_idx, f_idx))
+
+        # Hook up to source and sink
+        source_idx = 0
+        for idx in range(1, num_crews + 1):
+            g.add_edge(source_idx, idx, 1)
+            # print('added edge from {0} to {1}'.format(source_idx, idx))
+
+        sink_idx = g.num_vertices() - 1
+        #                /- first of flights  /- last idx of flights
+        for idx in range(num_crews + 1, sink_idx):
+            g.add_edge(idx, sink_idx, 1)
+            # print('added edge from {0} to {1}'.format(idx, sink_idx))
+        
+        return g
+
+        '''n = len(adj_matrix)
         m = len(adj_matrix[0])
         matching = [-1] * n
         busy_right = [False] * m
@@ -149,11 +225,34 @@ class MaxMatching:
                    (not busy_right[j])):
                         matching[i] = j
                         busy_right[j] = True
-        return matching
+        return matching'''
+    
+    def find_matching(_, graph, bipartition):
+        # call max flow on graph
+        (_, g_prime) = max_flow(graph, 0, graph.num_vertices())
+        (l, r) = bipartition
+        # assume that crews are on left, flights on right
+        matches = []
+        # iterate over flight vertices
+        for vertex in range(l + 1, l + r + 1):
+            for (dest, _) in graph.get_edge_indices(vertex):
+                # check if edge ends inside crews partition
+                # print('got an edge ending at {}'.format(dest))
+                if (1 <= dest <= l):
+                    # print('adding it!')
+                    matches.append(dest)
+                    break
+            else:
+                matches.append(-1)
+        # print('matches: {}'.format(matchesp))
+        return matches
 
     def solve(self):
         adj_matrix = self.read_data()
-        matching = self.find_matching(adj_matrix)
+        graph = self.build_graph(adj_matrix)
+        n_flights = len(adj_matrix)
+        n_crews = len(adj_matrix[0])
+        matching = self.find_matching(graph, (n_crews, n_flights))
         self.write_response(matching)
 
 if __name__ == '__main__':
--- AirlineTest.py
+++ AirlineTest.py
@@ -2,7 +2,16 @@ import unittest
 import Airline
 
 
-class TestMaxMatching(unittest.TestCase):
+def resolve_edges(graph, indices):
+        edges = []
+        for idx in indices:
+            e = graph.edges[idx]
+            edges.append((e.from_, e.to))
+        return edges
+
+
+class TestBreadthFirstSearch(unittest.TestCase):
+    
     def test_breadth_first_search(self):
         f = Airline.FlowGraph(4)
         f.add_edge(0, 1, 1)
@@ -10,24 +19,61 @@ class TestMaxMatching(unittest.TestCase):
         f.add_edge(1, 3, 1)
         f.add_edge(2, 3, 1)
 
-        p = f.breadth_first_search(0, 3)
-        edges = []
-        for idx in p:
-            e = f.edges[idx]
-            edges.append((e.from_, e.to))
-
-        expect = [(0, 1), (1, 3)]
-
+        (flow, path) = Airline.breadth_first_search(f, 0, 3)
+        
+        self.assertEqual(flow, 1)
+        edges = resolve_edges(f, path)
+        expect = [(0, 2), (2, 3)]
+        self.assertEqual(edges, expect)
+    
+    def test_another_breadth_first_search(self):
+        f = Airline.FlowGraph(4)
+        f.add_edge(0, 1, 10000)
+        f.add_edge(0, 2, 10000)
+        f.add_edge(1, 2, 1)
+        f.add_edge(1, 3, 10000)
+        f.add_edge(2, 3, 10000)
+        
+        (flow, path) = Airline.breadth_first_search(f, 0, 3)
+        
+        self.assertEqual(flow, 10000)
+        edges = resolve_edges(f, path)
+        expect = [(0, 2), (2, 3)]
         self.assertEqual(edges, expect)
 
-    def test_easy_matching(self):
-        m = Airline.MaxMatching()
-        test_matrix = [(0, 1, 0),
-                       (1, 0, 1),
-                       (0, 1, 0)]
 
-        result = m.find_matching(test_matrix)
-        self.assertEqual([1, 0, -1], result)
+class TestMaxMatching(unittest.TestCase):
+    
+    def test_easy_matching(self):
+        print('\n\n\n-= BEGIN MATCH TEST =-')
+        m = Airline.MaxMatching()
+        test_matrix = [(1,)]
+        g = m.build_graph(test_matrix)
+        (flow, g_prime) = Airline.max_flow(g, 0, 3)
+        self.assertEqual(flow, 1)
+    
+    def test_medium_matching(self):
+        print('\n\n\n-= BEGIN MATCH TEST TWO =-')
+        m = Airline.MaxMatching()
+        test_matrix = [(1, 0, 0, 1),
+                       (0, 1, 0, 1),
+                       (0, 0, 1, 1)]
+        g = m.build_graph(test_matrix)
+        (flow, g_prime) = Airline.max_flow(g, 0, 8)
+        self.assertEqual(flow, 3)
+        matching = m.find_matching(g_prime, (4, 3))
+        self.assertEqual(matching, [1, 2, 3])
+        
+    def test_eg_one(self):
+        m = Airline.MaxMatching()
+        test_matrix = [(1, 1, 0, 1),
+                       (0, 1, 0, 0),
+                       (0, 0, 0, 0)]
+        g = m.build_graph(test_matrix)
+        (flow, g_prime) = Airline.max_flow(g, 0, 8)
+        self.assertEqual(flow, 2)
+        matching = m.find_matching(g_prime, (4, 3))
+        self.assertEqual(matching, [1, 2, -1])
 
 if __name__ == '__main__':
     unittest.main()
